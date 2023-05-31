b04919ee2ce877eb9491ca47fd1bee09f7bcfadb
Tuấn Nguyễn <tomtepfa@gmail.com>
2023-05-31 17:58:41 +0700

    Add deploy config

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/Chatbox/",
   plugins: [react()],
   optimizeDeps: {
     exclude: ['uuid']
